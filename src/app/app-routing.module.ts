01f88784acd4b4489fa9d389ddd2ef2131c9d938
JulianEcheverri <julianecheverri@outlook.com>
2019-11-18 13:32:34 -0500

    App init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule) // se especifica el modulo de las rutas por defecto
   }
 ];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    // preloadingStrategy configuracion por defecto que le dice al router de angular que cargue o precargue modulos de rutas
   ],
   exports: [RouterModule]
 })
