2df7a7beb9403d111e091f1f9a3f0339971d0d4f
JulianEcheverri <julianecheverri@outlook.com>
2019-11-19 14:33:36 -0500

    Adding data to localStorage

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@angular/core';
-import { ListaDeTareas } from '../models/listaDeTareas';
-
+import { Injectable } from "@angular/core";
+import { ListaDeTareas } from "../models/listaDeTareas";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TasksService {
   // aquie manejaremos las listas de las tareas
@@ -11,21 +10,32 @@ export class TasksService {
   // obtenemos las listas
   listasDeTareas: ListaDeTareas[] = [];
 
-
   // idea del servicio es manejar una unica instancia a lo largo de la aplicacion
-  constructor() 
-  { 
+  constructor() {
+    this.cargarStorage();
     // crear dos listas para las pruebas visuales
-    const lista1 = new ListaDeTareas('Recolectar priedas del infinito');
-    const lista2 = new ListaDeTareas('Heroes a desaparecer');
-    this.listasDeTareas.push(lista1,lista2);
+    // const lista1 = new ListaDeTareas("Recolectar priedas del infinito");
+    // const lista2 = new ListaDeTareas("Heroes a desaparecer");
+    // this.listasDeTareas.push(lista1, lista2);
     // console.log(this.listasDeTareas);
-    
   }
 
-  crearLista(titulo: string){
+  crearLista(titulo: string) {
     const nuevaLista = new ListaDeTareas(titulo);
     this.listasDeTareas.push(nuevaLista);
+    this.guardarStorage();
   }
 
+  guardarStorage() {
+    // solo gruadarn strings, debe ser json
+    // existen tres lugares donde se puede guardar la informacion y hacerla persistente
+    // localStorage en forma de string
+    // session storage, se guarda ahi pero se borra cuando la persona cierre el navegador
+    // coockies
+    localStorage.setItem('data', JSON.stringify(this.listasDeTareas));
+  }
+
+  cargarStorage() {
+    if (localStorage.getItem('data')) this.listasDeTareas = JSON.parse(localStorage.getItem('data'));
+  }
 }
